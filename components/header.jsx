63be853434fe66403941e245261bd4ae5a37e27c
h1a2r3s4h <harshitgangwar1601@gmail.com>
2025-10-31 10:15:01 +0530

    make changes in header

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "./ui/button";
 import {
   LayoutDashboard,
-  FileText,
+
   GraduationCap,
   ChevronDown,
   StarsIcon,
@@ -85,13 +85,7 @@ export default async function Header() {
                   <ChevronDown className="h-4 w-4" />
                 </Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-48">
-                <DropdownMenuItem asChild>
-                  <Link href="/resume" className="flex items-center gap-2">
-                    <FileText className="h-4 w-4" />
-                    Build Resume
-                  </Link>
-                </DropdownMenuItem>
+          
               
                 <DropdownMenuItem asChild>
                   <Link href="/interview" className="flex items-center gap-2">
