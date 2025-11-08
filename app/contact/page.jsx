9775ced92378fb581928d6f75a296c2f0886d247
Palak Gangwar <palakgang1505@gmail.com>
2025-11-08 21:32:13 +0530

    Add contact page with ContactForm component

---
+++ app/contact/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContactForm from '@/components/ContactForm'
+
+function page() {
+  return (
+    
+    <div className="min-h-screen bg-neutral-950 flex items-center justify-center p-4">
+        <ContactForm />
+    </div>
+  )
+}
+
+export default page
